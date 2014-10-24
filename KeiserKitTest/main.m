2499fc0808941d1be4e344e4a33a93d806e4d052
Ben Woodford <me@benwoodford.co.uk>
2014-10-24 12:18:52 +0100

    Initial Commit

---
+++ KeiserKitTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KeiserKitTest
+//
+//  Created by Ben Woodford on 24/10/2014.
+//  Copyright (c) 2014 Ben Woodford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
